c1d4fa465dcd4821f17bf8f20c806dd897a3efb3
lclscofield <lclscofield@foxmail.com>
2018-01-24 11:23:50 +0800

    feat: MessageWindow 基本结构

--- src/components/home/user/messageWindow/MessageWindow.vue
+++ src/components/home/user/messageWindow/MessageWindow.vue
@@ -1,13 +1,30 @@
 <template>
-    <div id="MessageWindow"></div>
+    <div id="messageWindow">
+        <div class="box">
+            <MessageShow />
+            <MessageInput />
+        </div>
+    </div>
 </template>
 
 <script>
+    import MessageShow from './MessageShow'
+    import MessageInput from './MessageInput'
     export default {
-        name: 'MessageWindow'
+        name: 'MessageWindow',
+        components: { MessageShow, MessageInput }
     }
 </script>
 
 <style lang="scss" scoped>
+    #messageWindow {
+      height: 100%;
 
+      > .box {
+        position: relative;
+        background-color: #eee;
+        height: 100%;
+        overflow: hidden;
+      }
+    }
 </style>
