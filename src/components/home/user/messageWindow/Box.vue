50bbea1dc9227498fe371714f4a375990c5ed8b9
lclscofield <lclscofield@foxmail.com>
2018-01-24 16:59:56 +0800

    refactor: 将 boxBody 独立为组件

--- src/components/home/user/messageWindow/Box.vue
+++ src/components/home/user/messageWindow/Box.vue
@@ -3,17 +3,12 @@
         <!-- begin head -->
         <BoxHead />
         <!-- end head -->
-        <div class="boxBody">
-            <div class="messages">
-                <div class="message"></div>
-            </div>
-            <div class="messageEmpty"
-                 v-if="noMessage">
-                <i v-if="showHint"></i>
-                <p v-if="!showHint">暂时没有新消息</p>
-                <p v-if="showHint">未选择聊天</p>
-            </div>
-        </div>
+        <!-- begin body -->
+        <BoxBody />
+        <!-- end body -->
+        <!-- begin input -->
+        <BoxInput />
+        <!-- end input -->
     </div>
 </template>
 
@@ -23,13 +18,7 @@
     import BoxInput from './BoxInput'
     export default {
         name: 'Box',
-        components: { BoxHead, BoxBody, BoxInput },
-        data () {
-            return {
-                noMessage: true, // 刚加载时显示空白页
-                showHint: false
-            }
-        }
+        components: { BoxHead, BoxBody, BoxInput }
     }
 </script>
 
@@ -39,32 +28,5 @@
       background-color: #eee;
       height: 100%;
       overflow: hidden;
-
-      > .boxBody {
-        position: absolute;
-        padding: 0 19px;
-        bottom: 180px;
-        top: 51px;
-        left: 0;
-        right: 0;
-        overflow-y: auto;
-        overflow-x: hidden;
-
-        > .messageEmpty {
-          text-align: center;
-          margin-top: 130px;
-          color: #ccc;
-          font-size: 13px;
-
-          > i {
-            display: inline-block;
-            vertical-align: middle;
-            width: 100px;
-            height: 90px;
-            background: url("//res.wx.qq.com/a/wx_fed/webwx/res/static/img/1OvE4o2.png")
-              0 -1332px;
-          }
-        }
-      }
     }
 </style>
--- src/components/home/user/messageWindow/BoxBody.vue
+++ src/components/home/user/messageWindow/BoxBody.vue
@@ -1,13 +1,54 @@
 <template>
-    <div id="MessageInput"></div>
+    <div id="boxBody">
+        <div class="messages">
+            <div class="message"></div>
+        </div>
+        <div class="messageEmpty"
+             v-if="noMessage">
+            <i v-if="showHint"></i>
+            <p v-if="!showHint">暂时没有新消息</p>
+            <p v-if="showHint">未选择聊天</p>
+        </div>
+    </div>
 </template>
 
 <script>
     export default {
-        name: 'MessageInput'
+        name: 'BoxBody',
+        data () {
+            return {
+                noMessage: true, // 刚加载时显示空白页
+                showHint: false
+            }
+        }
     }
 </script>
 
 <style lang="scss" scoped>
+    #boxBody {
+      position: absolute;
+      padding: 0 19px;
+      bottom: 180px;
+      top: 51px;
+      left: 0;
+      right: 0;
+      overflow-y: auto;
+      overflow-x: hidden;
 
+      > .messageEmpty {
+        text-align: center;
+        margin-top: 130px;
+        color: #ccc;
+        font-size: 13px;
+
+        > i {
+          display: inline-block;
+          vertical-align: middle;
+          width: 100px;
+          height: 90px;
+          background: url("//res.wx.qq.com/a/wx_fed/webwx/res/static/img/1OvE4o2.png")
+            0 -1332px;
+        }
+      }
+    }
 </style>
