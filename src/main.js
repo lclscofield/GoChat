d2f8c384d75d48a85846160e9ea2efabe539a1fd
lclscofield <lclscofield@foxmail.com>
2018-01-24 11:21:51 +0800

    feat: 引入 vuex

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import axios from 'axios'
+import store from './vuex'
 import './assets/reset.scss'
 
 Vue.config.productionTip = false
@@ -14,6 +15,7 @@ Vue.prototype.$axios = axios
 new Vue({
     el: '#app',
     router,
+    store,
     template: '<App/>',
     components: {
         App
