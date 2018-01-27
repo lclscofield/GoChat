60ccc54b6c1cbaa3dd9d2985a528a352281334e3
lclscofield <lclscofield@foxmail.com>
2018-01-27 17:53:13 +0800

    feat: 后端引入 body-parser 模块解析 post 请求

--- server/index.js
+++ server/index.js
@@ -1,10 +1,21 @@
-var express = require('express')
-var app = express()
+const express = require('express')
+const path = require('path')
+const api = require('./api')
+const bodyParser = require('body-parser') // 解析 post 请求的中间件
+const app = express()
 
-app.get('/', function (req, res) {
-    res.send('xxx')
+app.set('port', (process.env.port || 3000))
+app.use(express.static(path.join(__dirname, '../dist')))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+    extended: false
+}))
+app.use(api)
+
+app.get('/', (req, res) => {
+    res.send('404')
 })
 
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!')
+app.listen(app.get('port'), () => {
+    console.log('Visit http://localhost:' + app.get('port'))
 })
