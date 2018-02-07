b38bda723c1b9add4947f801937c0206112f41e7
lclscofield <lclscofield@foxmail.com>
2018-02-07 13:55:50 +0800

    style: 调整格式

--- server/api.js
+++ server/api.js
@@ -4,36 +4,23 @@ const db = require('./db')
 
 router.post('/signUp', (req, res) => {
     console.log(req.body)
-    const {
-        username,
-        phone
-    } = req.body
-    db.UserInfo.findOne({
-        username
-    }, (err, doc) => {
+    const { username, phone } = req.body
+    db.UserInfo.findOne({ username }, (err, doc) => {
         if (err) {
             console.log(err)
         } else if (doc) {
-            res.send({
-                errType: 'username'
-            })
+            res.send({ errType: 'username' })
         } else {
-            db.UserInfo.findOne({
-                phone
-            }, (err, doc) => {
+            db.UserInfo.findOne({ phone }, (err, doc) => {
                 if (err) {
                     console.log(err)
                 } else if (doc) {
-                    res.send({
-                        errType: 'phone'
-                    })
+                    res.send({ errType: 'phone' })
                 } else {
                     db.UserInfo(req.body).save(err => {
                         console.log(err)
                     })
-                    res.send({
-                        type: 'success'
-                    })
+                    res.send({ type: 'success' })
                 }
             })
         }
