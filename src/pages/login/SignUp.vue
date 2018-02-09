1061e91ae1a90a7f7261ec76b98a2acb5c099a52
lclscofield <lclscofield@foxmail.com>
2018-02-09 14:55:51 +0800

    style: 调整格式

--- src/pages/login/SignUp.vue
+++ src/pages/login/SignUp.vue
@@ -129,21 +129,20 @@
                         password: this.inputPassword,
                         phone: this.inputPhone
                     }
-                    this.signUpVerify(postData)
-                        .then((data) => {
-                            console.log(data)
-                            if (data.type === 'success') {
-                                this.signUpSuccess = true
-                                this.isClick = false
-                                setTimeout(() => {
-                                    this.leave()
-                                }, 1500)
-                            } else if (data.errType === 'username') {
-                                this.usernameHint = '用户名已被注册'
-                            } else if (data.errType === 'phone') {
-                                this.phoneHint = '手机号已被注册'
-                            }
-                        })
+                    this.signUpVerify(postData).then((data) => {
+                        console.log(data)
+                        if (data.type === 'success') {
+                            this.signUpSuccess = true
+                            this.isClick = false
+                            setTimeout(() => {
+                                this.leave()
+                            }, 1500)
+                        } else if (data.errType === 'username') {
+                            this.usernameHint = '用户名已被注册'
+                        } else if (data.errType === 'phone') {
+                            this.phoneHint = '手机号已被注册'
+                        }
+                    })
                 }
             },
             leave () {
