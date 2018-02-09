e43ca55c0c93319c53eb857aa9f5f9130453ceb3
lclscofield <lclscofield@foxmail.com>
2018-02-09 13:59:33 +0800

    refactor: 优化注册处理程序

--- server/api.js
+++ server/api.js
@@ -5,21 +5,18 @@ const db = require('./db')
 router.post('/signUp', (req, res) => {
     const { username, phone } = req.body
     db.UserInfo.count({ username }, (err, count) => {
-        if (err) {
-            console.log(err)
-        } else if (count) {
+        if (!err && count) {
             res.send({ errType: 'username' })
+        } else {
+            db.UserInfo.count({ phone }, (err, count) => {
+                if (!err && count) {
+                    res.send({ errType: 'phone' })
+                } else {
+                    db.UserInfo.create(req.body)
+                    res.send({ type: 'success' })
+                }
+            })
         }
-        db.UserInfo.count({ phone }, (err, count) => {
-            if (err) {
-                console.log(err)
-            } else if (count) {
-                res.send({ errType: 'phone' })
-            } else {
-                db.UserInfo.create(req.body)
-                res.send({ type: 'success' })
-            }
-        })
     })
 })
 
